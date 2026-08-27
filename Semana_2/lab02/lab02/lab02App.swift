4c92b500240ba2f95493573e4a5c7edd925bb165
José Alonso <jose.lopez.car@tecsup.edu.pe>
2026-08-27 11:51:49 -0500

    Lab02

---
+++ Semana_2/lab02/lab02/lab02App.swift
@@ -0,0 +1,17 @@
+//
+//  lab02App.swift
+//  lab02
+//
+//  Created by Tecsup on 27/08/26.
+//
+
+import SwiftUI
+
+@main
+struct lab02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

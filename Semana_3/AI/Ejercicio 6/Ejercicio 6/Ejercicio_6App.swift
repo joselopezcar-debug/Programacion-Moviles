cb3bdb7fdf3d6cf34089ad88773738a641a2a8f4
Jospe Alonso <jose.lopez.car@tecsup.edu.pe>
2026-09-03 13:54:02 -0500

    Ejercicio 6: Gestión de notas - IA

---
+++ Semana_3/AI/Ejercicio 6/Ejercicio 6/Ejercicio_6App.swift
@@ -0,0 +1,17 @@
+//
+//  Ejercicio_6App.swift
+//  Ejercicio 6
+//
+//  Created by Tecsup on 3/09/26.
+//
+
+import SwiftUI
+
+@main
+struct Ejercicio_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

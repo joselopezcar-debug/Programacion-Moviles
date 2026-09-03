56736843379e301f207179b33c3f3155122828c1
Jospe Alonso <jose.lopez.car@tecsup.edu.pe>
2026-09-03 12:11:19 -0500

    Ejercicio 3

--- Semana_2/Ejercicio 2/Ejercicio 2.2.playground/Contents.swift
+++ Semana_2/Ejercicio 2/Ejercicio 2.2.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// PREDICT 4: "28 o 29 días" (febrero)
+let mes = 2
+switch mes {
+case 1, 3, 5, 7, 8, 10, 12:
+    print("31 días")
+case 4, 6, 9, 11:
+    print("30 días")
+case 2:
+    print("28 o 29 días")
+default:
+    print("Mes inválido")
+}
+
+// PREDICT 5: "Vocal" ("a" es vocal)
+let letra: Character = "a"
+switch letra {
+case "a", "e", "i", "o", "u":
+    print("Vocal")
+default:
+    print("Consonante")
+}

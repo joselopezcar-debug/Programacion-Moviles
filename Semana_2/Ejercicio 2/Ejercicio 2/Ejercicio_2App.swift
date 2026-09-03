4bb9204294b859574e56001a7291a34c212fc736
Jospe Alonso <jose.lopez.car@tecsup.edu.pe>
2026-09-03 12:06:27 -0500

    Ejercicio 2

---
+++ Semana_2/Ejercicio 2/Ejercicio 2/Ejercicio_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Ejercicio_2App.swift
+//  Ejercicio 2
+//
+//  Created by Tecsup on 3/09/26.
+//
+
+import SwiftUI
+
+@main
+struct Ejercicio_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
